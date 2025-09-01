6db100315bd92a6bd21bd17d5b485a846532cb6f
Jaedyncorbin <jcorb12@lsu.edu>
2025-08-31 19:58:58 -0500

    ellipse assignment

---
+++ sketch.js
@@ -0,0 +1,159 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0, 0, 0);
+  noStroke();
+  
+fill(255); 
+ellipse(200, 200, 25, 150); 
+
+fill(255); 
+ellipse(200, 200, 150, 25); 
+
+fill(255); 
+ellipse(45, 70, 95, 95); 
+
+fill(0); 
+ellipse(15, 70, 95, 95); 
+
+fill(255); 
+ellipse(355, 70, 95, 95); 
+
+fill(0); 
+ellipse(385, 70, 95, 95); 
+
+fill(255); 
+ellipse(45, 330, 95, 95); 
+
+fill(0); 
+ellipse(15, 330, 95, 95); 
+
+fill(255); 
+ellipse(355, 330, 95, 95); 
+
+fill(0); 
+ellipse(385, 330, 95, 95); 
+
+fill(255); 
+ellipse(155, 155, 10, 110); 
+
+fill(255); 
+ellipse(155, 155, 110, 10); 
+
+fill(255); 
+ellipse(245, 245, 10, 110); 
+
+fill(255); 
+ellipse(245, 245, 110, 10); 
+
+fill(0);
+ellipse(200, 200, 10, 300);
+
+fill(0); 
+ellipse(200, 200, 300, 10); 
+
+
+
+fill(255);
+ellipse(180, 30, 10, 40);
+
+fill(255);
+ellipse(180, 30, 40, 10);
+
+fill(0);
+ellipse(180, 30, 40, 3);
+
+fill(0);
+ellipse(180, 30, 3, 40);
+
+
+
+
+fill(255);
+ellipse(220, 30, 10, 40);
+
+fill(255);
+ellipse(220, 30, 40, 10);
+
+fill(0);
+ellipse(220, 30, 3, 40);
+
+fill(0);
+ellipse(220, 30, 40, 3);
+
+
+
+
+
+
+fill(255);
+ellipse(180, 370, 10, 40);
+
+fill(255);
+ellipse(180, 370, 40, 10);
+
+fill(0);
+ellipse(180, 370, 3, 40);
+
+fill(0);
+ellipse(180, 370, 40, 3);
+
+fill(255);
+ellipse(220, 370, 10, 40);
+
+fill(255);
+ellipse(220, 370, 40, 10);
+
+fill(0);
+ellipse(220, 370, 3, 40);
+
+fill(0);
+ellipse(220, 370, 40, 3);
+
+
+
+fill(255); 
+ellipse(45, 170, 35, 35); 
+
+fill(0); 
+ellipse(55, 170, 35, 35); 
+
+fill(255); 
+ellipse(45, 230, 35, 35); 
+
+fill(0); 
+ellipse(55, 230, 35, 35); 
+
+fill(255); 
+ellipse(355, 230, 35, 35); 
+
+fill(0); 
+ellipse(345, 230, 35, 35); 
+
+fill(255); 
+ellipse(355, 170, 35, 35); 
+
+
+
+fill(0); 
+ellipse(345, 170, 35, 35); 
+
+
+
+fill(255);
+ellipse(125, 370, 15, 15);
+
+fill(255);
+ellipse(275, 370, 15, 15);
+
+fill(255);
+ellipse(125, 30, 15, 15);
+
+fill(255);
+ellipse(275, 30, 15, 15);
+
+
+
+}
